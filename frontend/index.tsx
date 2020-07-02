d2f88ddb665b311e836b347de90f9bb3f3634ac6
Mikhail Lazarev <mikael.lazarev@gmail.com>
2020-07-02 14:52:46 +0300

    Design skeleton

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,9 +1,83 @@
-import {initializeBlock} from '@airtable/blocks/ui';
-import React from 'react';
+import {
+  Box,
+  Button,
+  Heading,
+  initializeBlock,
+  TextButton,
+} from "@airtable/blocks/ui";
+import React from "react";
+import { viewport } from "@airtable/blocks";
 
-function HelloWorldTypescriptBlock() {
-    // YOUR CODE GOES HERE
-    return <div>Hello world 🚀</div>;
+// Determines the maximum size of the block in fullscreen mode.
+viewport.addMaxFullscreenSize({
+  height: 620,
+  width: 840,
+});
+
+viewport.addMinSize({
+  height: 260,
+  width: 400,
+});
+
+function App() {
+  // YOUR CODE GOES HERE
+  return (
+    <>
+      <Box
+        position="absolute"
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Box
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          display="flex"
+          flexDirection="row"
+          alignContent="center"
+          alignItems="center"
+          justifyContent="space-between"
+          width={"100%"}
+          paddingLeft={"20px"}
+          paddingRight={"20px"}
+          paddingTop={"8px"}
+          paddingBottom={"8px"}
+          backgroundColor={"#8ca5ff"}
+        >
+          <Heading size={"xsmall"}>AirLive</Heading>
+          <Box>
+            <Button marginRight={"10px"}>Settings</Button>
+            <Button>Publish</Button>
+          </Box>
+        </Box>
+        <Box display="flex"
+             flexDirection="row"
+             alignContent="center"
+             alignItems="center"
+             justifyContent="space-between"
+             width={"100%"}
+             paddingLeft={"20px"}
+             paddingRight={"20px"}
+             paddingTop={"8px"}
+             paddingBottom={"8px"}
+             backgroundColor={"#8ca5ff"}
+        >
+          <Heading size={"xsmall"}>AirLive</Heading>
+          <Box>
+        <Button onClick={() => console.log("erer")} marginRight={"10px"}>Prev</Button>
+        <Button onClick={() => console.log("erer")}>Next</Button>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 }
 
-initializeBlock(() => <HelloWorldTypescriptBlock />);
+initializeBlock(() => <App />);
